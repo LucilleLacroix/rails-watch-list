cf62ab5c3e47860b56b899d16c580b7a03bc83b2
Lucille <lucille.lacroix74520@gmail.com>
2025-08-25 08:32:41 +0200

    -copie corected

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,24 @@ Rails.application.routes.draw do
   # root "posts#index"
   root to: "lists#index"
 
-  resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:new, :create, :destroy]
-  end
+  get "/lists", to: "lists#index", as: :lists
+  get "/lists/new", to: "lists#new", as: :new_list
+  post "/lists", to: "lists#create"
+  get "/lists/:id", to: "lists#show", as: :list
+  get "/lists/:id/edit", to: "lists#edit", as: :edit_list
+  patch "/lists/:id", to: "lists#update"
+  put "/lists/:id", to: "lists#update"
+  delete "/lists/:id", to: "lists#destroy"
+
+
+  get "/lists/:list_id/bookmarks/new", to: "bookmarks#new", as: :new_list_bookmark
+
+  post "/lists/:list_id/bookmarks", to: "bookmarks#create", as: :list_bookmarks
+
+  get "/lists/:list_id/bookmarks/:id/edit", to: "bookmarks#edit", as: :edit_list_bookmark
+
+  patch "/lists/:list_id/bookmarks/:id", to: "bookmarks#update"
+  put "/lists/:list_id/bookmarks/:id", to: "bookmarks#update"
+
+  delete "/lists/:list_id/bookmarks/:id", to: "bookmarks#destroy", as: :list_bookmark
 end
